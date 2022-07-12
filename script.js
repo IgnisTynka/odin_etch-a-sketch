70e2fff0ff06a81269f5ebcb21c2bff39cd75351
Tynka <marcia03835@gmail.com>
2022-07-12 15:47:23 +0200

    Final version

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function createBoard(container, boardSize){
+    container.innerHTML = '';
+    let size = 600/boardSize;
     for(i=0; i< boardSize*boardSize; i++){
         let field = document.createElement('div')
+        field.style.width = size + 'px';
+        field.style.height = size + 'px';
         field.classList.add('field');
         container.appendChild(field);
+    }
+    let fields = document.querySelectorAll('.field');
+    for (const field of fields){
+        field.addEventListener('mouseenter', () => {
+            field.style.backgroundColor = color.value;
+        })    
     }
 }
 
@@ -10,10 +20,15 @@ let container = document.querySelector('.container');
 
 createBoard(container, 16);
 
-let fields = document.querySelectorAll('.field')
+set.addEventListener('click', () => {
+    let boardSize = Number(document.getElementById('sizeValue').value);
+    createBoard(container, boardSize);
+    }
+)
 
-for (const field of fields){
-    field.addEventListener('mouseenter', () => {
-        field.style.backgroundColor = 'black';
-    })    
-}
+clear.addEventListener('click', () => {
+    let fields = document.querySelectorAll('.field');
+    for (const field of fields){
+        field.style.backgroundColor = '#FDF0D5';
+    }
+})
